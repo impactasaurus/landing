aad19d879f8af631bf65776a04a35560fd21fa4e
drimpact <drimpact@users.noreply.github.com>
2018-11-20 22:26:27 +0000

    import cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Link } from "gatsby";
 import * as React from "react";
 import {Menu} from "./Menu";
-import SEO from "./SEO";
 import { Segment, Icon, Container } from "semantic-ui-react";
 import { Provider } from "react-redux";
 import { store } from "../store";
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import { get } from "lodash";
-import { Header, Container, Segment, Icon, Label, Button, Grid, Card, Image, Item, Comment } from "semantic-ui-react";
-import { MarkdownRemark, ImageSharp, MarkdownRemarkConnection, Site } from "../graphql-types";
+import { Header, Container, Segment, Label, Grid, Card, Image, Item, Comment } from "semantic-ui-react";
+import { MarkdownRemark, ImageSharp, MarkdownRemarkConnection } from "../graphql-types";
 import BlogTitle from "../components/BlogTitle";
 import {withLayout, LayoutProps} from "../components/Layout";
 import { graphql } from "gatsby";
