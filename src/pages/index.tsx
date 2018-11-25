bcd38c73a2746095da036a113bce8cdc76f29ed3
drimpact <drimpact@users.noreply.github.com>
2018-11-25 12:37:53 +0000

    sorting robots.txt and sitemap. adding SEO to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { withLayout, LayoutProps } from "../components/Layout";
 import Signup from "../components/Signup";
+import SEO from "../components/SEO";
 import Testimonials from "../components/Testimonials";
 import Steps from "../components/Steps";
 import IndexHero from "../components/IndexHero";
 
 const IndexPage = (props: LayoutProps) => (
   <div>
+    <SEO />
     <IndexHero />
     <Steps />
     <Testimonials />
