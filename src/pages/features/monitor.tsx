442ba79d2b69d9615c2ed50a4e9e3e13a3a38761
drimpact <drimpact@users.noreply.github.com>
2018-12-29 13:26:06 +0000

    adding monitor feature page

--- src/pages/features/monitor.tsx
+++ src/pages/features/monitor.tsx
@@ -1,15 +1,40 @@
 import * as React from "react";
 import {withLayout} from "../../components/Layout";
+import DescribedImage from "../../components/DescribedImage";
+import Container from "react-bootstrap/lib/Container";
+import Hero from "../../components/Hero";
+import SEO from "../../components/SEO/SEO";
 
 const JourneyPage = () => {
   return (
-    <div>
-      <ul>
-        <li>View individual beneficiary, radar or line chart</li>
-        <li>Adjust service to optimise for the individual</li>
-        <li>Manage records</li>
-      </ul>
-    </div>
+    <>
+    <SEO title="Monitor"/>
+    <Hero>
+      <h1>Monitor</h1>
+    </Hero>
+    <Container className="slanted">
+      <DescribedImage
+        image="/images/screenshots/impactasaurus-graph.png"
+        title="Journey"
+        desc={(
+          <p>
+            Your beneficiaries go through a journey with your support. Understand their journey by visualising an individual beneficiary's records.
+          </p>
+        )}
+        odd={true}
+      />
+      <DescribedImage
+      image="/images/screenshots/table.png"
+      title="Optimise service"
+      desc={(
+        <p>
+          Once a beneficiary's journey can be visualised, the service you provide can be optimised for the individual. If you can't measure it, you can't improve it.
+        </p>
+      )}
+      odd={false}
+      />
+    </Container>
+    </>
   );
 };
 
