b2de45a10a5d185fd477f5ca63937a0fd021bffe
drimpact <drimpact@users.noreply.github.com>
2018-12-29 15:05:20 +0000

    report feature page

--- src/pages/features/reporting.tsx
+++ src/pages/features/reporting.tsx
@@ -1,15 +1,51 @@
 import * as React from "react";
 import {withLayout} from "../../components/Layout";
+import DescribedImage from "../../components/DescribedImage";
+import Container from "react-bootstrap/lib/Container";
+import Hero from "../../components/Hero";
+import SEO from "../../components/SEO/SEO";
+import { Link } from "gatsby";
 
 const ReportingPage = () => {
   return (
-    <div>
-      <ul>
-        <li>Radar graph across beneficiaries</li>
-        <li>Segment by tag</li>
-        <li>Export data</li>
-      </ul>
-    </div>
+    <>
+    <SEO title="Reporting"/>
+    <Hero>
+      <h1>Reporting</h1>
+    </Hero>
+    <Container className="slanted">
+      <DescribedImage
+        image="/images/screenshots/report.png"
+        title="Visualise your impact"
+        desc={(
+          <p>
+            View the average distance travelled by your beneficiaries, as a result of your intervention. Download visualisations to include in your impact reports.
+          </p>
+        )}
+        odd={true}
+      />
+      <DescribedImage
+        image="/images/screenshots/report-tags.png"
+        title="Report on a subset of beneficiaries"
+        desc={(
+          <p>
+            Generate reports for a particular project, location or demographic using tags. See the <Link to={"/features/segments"}>segments</Link> feature page for more information.
+          </p>
+        )}
+        odd={false}
+      />
+      <DescribedImage
+        image="/images/screenshots/report-export.png"
+        title="Export the report's data"
+        desc={(
+          <p>
+            The data used to produce the report can be exported to Excel for backup, verification or custom analysis.
+          </p>
+        )}
+        odd={true}
+      />
+    </Container>
+    </>
   );
 };
 
